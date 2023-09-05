00600e9d37e246ed6237aff51c4f2d282043131a
Fedor <Fedor@DESKTOP-TGO8AHQ>
2023-09-05 13:18:40 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	int i = 0; int j = 0;
+
+	int n; cin >> n;
+
+	//O(2^n)
+
+	for (int i = 1; i < n; ++i)
+	{
+		// 1 2 4 8 16   2^n
+		for (int j = 0; j < pow(2, i); ++j)
+		{
+			cout << "+";
+		}
+	}
+
+	//1 + 2 + 4 + 8 + .. + 2^n = 2^(n+1) - 1
+	// 1 = 1
+	// 1 + 2 = 3
+	// 7
+	// 15
+	// 31
+	// 63
+
+
+
+
+}
+
+
+
+//
+//int main()
+//{
+//	// Time 
+//	//// Memory
+//
+//	int n;
+//	int k;
+//	std::cin >> n >> k;
+//
+//	// 1000 1000
+//
+//	// O(n)
+//	for (int i = 0; i < n; ++i)
+//	{
+//
+//
+//	}
+//	//1000
+//}
---
+++ Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main2()
+{
+	// Time 
+	//// Memory
+
+	int n;
+	int k;
+
+	std::cin >> n >> k;
+	// 1000 1000
+
+	// 1 2 4 8 16  1000000
+
+
+	// Ω(n)
+	// O(n^2)
+	// 
+
+	for (int i = 1; i < n * k; i *= 2)
+	{
+		std::cout << "Hello";
+	}
+
+
+
+	//20
+
+
+
+
+}
